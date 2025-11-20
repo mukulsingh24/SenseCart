1d49e1d3341f69f0762c7ac4332929d456dd99a7
mukulsingh24 <rmks1004@gmail.com>
2025-11-20 21:32:46 +0530

    created some pages

---
+++ frontend/src/pages/homepage.jsx
@@ -0,0 +1,21 @@
+import React, { useState,useEffect } from "react";
+import { BrowserRouter,Routes } from "react-router-dom"
+
+function Home(){
+    const[products,setProducts] = useState([]);
+    useEffect(
+        const obj = {
+            _id : "01",
+            name:Keyboard,
+            image: <img src="dummy.jpg" alt="blank page" />,
+            price:2000,
+        }
+    )
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Home

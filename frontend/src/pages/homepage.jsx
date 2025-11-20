2ccc62d9a66f38233b9ed432bf5afcac2139630f
mukulsingh24 <rmks1004@gmail.com>
2025-11-20 22:06:14 +0530

    homepage with dummy data created

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -3,17 +3,23 @@ import { BrowserRouter,Routes } from "react-router-dom"
 
 function Home(){
     const[products,setProducts] = useState([]);
-    useEffect(
-        const obj = {
+    useEffect(()=>{
+        const obj = [{
             _id : "01",
-            name:Keyboard,
-            image: <img src="dummy.jpg" alt="blank page" />,
-            price:2000,
-        }
-    )
+            name:"Keyboard",
+            image:"url",
+            price:2000
+        }]
+        setProducts(obj)
+    },[])
     return(
         <div>
-
+            <ul>
+            {products.map((item,index) => (
+                <li key={index}>{item.name},{item.price}
+                </li>
+            ))}
+            </ul>
         </div>
     )
 }

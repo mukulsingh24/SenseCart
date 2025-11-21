a828c471b1e47d7b5878b37c75268cd6e8b7f97f
mukulsingh24 <rmks1004@gmail.com>
2025-11-21 18:00:47 +0530

    cart adding working

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -1,10 +1,7 @@
 import React, { useState,createContext, Children } from "react";
-
 export const CartContext = createContext();
-
 export const CartProvider = ({children}) =>{
     const[cart,setCart] = useState([])
-    
     const addtoCart = (product) =>{
         setCart([...cart,product])
         alert("Added to Cart")

c1cb042f6464357eaede5992d69af9b8a49cdee5
mukulsingh24 <rmks1004@gmail.com>
2025-11-21 12:04:08 +0530

    bcrypt password done

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const UserSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -19,4 +20,20 @@ const UserSchema = new mongoose.Schema({
         default:"user"
     },
 },{ timestamps: true })
+
+UserSchema.pre('save', async function pass(next){
+    if(!this.isModified('password')){
+        return next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(this.password,salt)
+    this.password=hash
+    next()
+})
+
+UserSchema.methods.comparePassword = async function check(candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
+    return isMatch;
+}
+
 module.exports = mongoose.model('User',UserSchema)
